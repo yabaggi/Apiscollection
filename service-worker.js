2a6fb767c9d5d63cb6ffe58d574d03d59cb2d616
yabaggi <yabaggi@gmail.com>
2025-11-23 19:55:08 +0400

    icons

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,9 @@
 const CACHE_NAME = "apis-pwa-cache-v1";
 const OFFLINE_URLS = [
   "index.html",
-  "manifest.json"
+  "manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 // Install event: cache app shell
